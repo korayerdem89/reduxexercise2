fc316ff1a743baf52f32d41540395b1273287f65
Koray Erdem KILIÇ <developerkoray@gmail.com>
2021-08-10 21:10:15 +0300

    Router structure is wrapped with Provider

--- src/Router.js
+++ src/Router.js
@@ -4,20 +4,27 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Primary from './pages/Primary';
 import Secondary from './pages/Secondary';
+import UserProvider from './context/Provider.js';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Primary" component={Primary} />
-        {/* options={{
-           headerShown:false,  //options kullanımı önemli.dökümandan detaylı incele!
-         }} */}
-        <Tab.Screen name="Secondary" component={Secondary} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Primary" component={Primary} 
+          options={{
+          headerShown:false,  //options kullanımı önemli.dökümandan detaylı incele!
+        }}
+        />
+          <Tab.Screen name="Secondary" component={Secondary}
+            options={{
+              headerShown:false, 
+            }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 };
 
